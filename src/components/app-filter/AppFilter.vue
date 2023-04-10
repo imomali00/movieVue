bd880dbf2ea31fab0519eb0427b5adbe27156fcb
Macbook Air <macbookair@MacBook-Air-Macbook-2.local>
2023-04-10 22:13:32 +0500

    a

---
+++ src/components/app-filter/AppFilter.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="bnt-group">
+    <button
+      v-for="btn in filterButtons"
+      :key="btn.name"
+      class="btn"
+      :class="[filterName === btn.name ? 'btn-dark' : 'btn-outline-dark']"
+      @click="filterHandler(btn.name)"
+    >
+      {{ btn.title }}
+    </button>
+    <!-- <button
+      class="btn"
+      type="button"
+      @click="filterHandler('all')"
+      :class="[filterName === 'all' ? ' btn-dark' : ' btn-outline-dark']"
+    >
+      Barcha kinolar
+    </button>
+    <button
+      class="btn"
+      type="button"
+      @click="filterHandler('popular')"
+      :class="[filterName === 'popular' ? ' btn-dark' : ' btn-outline-dark']"
+    >
+      Mashhur kinolar
+    </button>
+    <button
+      class="btn"
+      type="button"
+      @click="filterHandler('mostViewrs')"
+      :class="[filterName === 'mostViewrs' ? ' btn-dark' : ' btn-outline-dark']"
+    >
+      Eng ko'p ko'rilgan kinolar
+    </button> -->
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    updateFilterHandler: {
+      type: Function,
+      required: true,
+    },
+    filterName: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      filterButtons: [
+        {
+          title: "Barcha kinolar",
+          name: "all",
+        },
+        {
+          title: " Mashhur kinolar",
+          name: "popular",
+        },
+        {
+          title: "Eng ko'p ko'rilgan kinolar",
+          name: "mostViewrs",
+        },
+      ],
+      filter: "all",
+    };
+  },
+  methods: {
+    filterHandler(filter) {
+      this.filter = filter;
+      this.updateFilterHandler(this.filter);
+    },
+  },
+};
+</script>
+<style></style>
